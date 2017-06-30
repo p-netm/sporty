9c5ca616c905ddb3d100c42b03c272f031ba3f82
= <pmuriuki98@gmail.com>
2017-06-30 23:40:15 +0300

    [Feature]

--- app/scrapper.py
+++ app/scrapper.py
@@ -2,11 +2,10 @@ import requests
 import os
 from bs4 import BeautifulSoup
 from datetime import datetime
-from .dataModel import Team, BASE
 import re
 import time as Time
 
-def scrap_for_mutual_science(url):
+def scrap_for_mutual_matches(url):
     """
     {
         "time": 12321321331,
@@ -54,127 +53,172 @@ def scrap_for_mutual_science(url):
     for tag in tbody_list:
         td_list.extend(tag.find_all('a', class_='tabOdds'))
 
-    all_hrefs = list()
-    money_list = list()
+
     flag_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                  'files', 'flagged', str(int(Time.time())) + '.txt'))
 
-            # create a txt file and add the name of the team
-    file_handler = open(flag_dir, 'w')
-    file_handler.write(" \n".join(money_list))
-    file_handler.close()
-
+    all_hrefs = list()
     for link in td_list:
         all_hrefs.append(link.get('href'))
 
     for link in all_hrefs:
-        home_and_away = get_specific_match_details(link)  # returns a list containing the home team details in the first
-        # index
-        home = home_and_away[0]
-        away = home_and_away[1]
+        parent_match = get_specific_match_details(link)
+        uncle_match = retrieve_mutual_matches_data(link)
+        match = parent_match.update(uncle_match)
+        start_conveyer(match)
 
-        # a record that has not yet been analyzed
-        home_probs = start_conveyer(home['team'])
-        away_probs = start_conveyer(away['team'])
-        # if home_probs['score_prob'] > 60 or home_probs['concede_prob'] > 60:
-        #     home_team.flagged = 1
-        # elif home_probs['score_prob'] <40 or home_probs['concede_prob'] <40:
-        #     home_team.flagged = -1
-        # if away_probs['score_prob'] > 60 or away_probs['concede_prob'] > 60:
-        #     away_team.flagged = 1
-        # elif away_probs['score_prob'] <40 or away_probs['concede_prob'] <40:
-        #     away_team.flagged = -1
 
-def start_conveyer(team_name):
-    num_of_records = 5
-    def _check(goals):
-        if goals > 5 :
-            weight = 5
-        elif goals > 4:
-            weight = 4
-        elif goals > 3:
-            weight = 3
-        elif goals < 2:
-            weight = 1
-        return weight
-    # first iteration is for each respective teams goals scored
-    def cumulative_goals(team):
-        team_goals_scored = 0
-        team_goals_conceded = 0
-        match_scores = 0
-        for a, b, c in team: # do the same for away_team
-            team_goals_scored += b
-            team_goals_conceded += c
-            match_scores += b + c
+def splitter(scores):
+    """ returns the scores in the desired integer format"""
+    scores = scores.split('-')
+    home_goals = int(scores[0])
+    away_goals = int(scores[1])
+    return {'home_goals': home_goals, 'away_goals': away_goals}
 
-        return {'total_scored': team_goals_scored, 'total_conceded': team_goals_conceded, 'total': match_scores}
-    # average = divide the sums by the number of records specified in the query offset
-    # team_goal_scored_spread = cumulative_goals(team)['total_scored'] / num_of_records
-    # team_goal_conceded_spread = cumulative_goals(team)['total_conceded'] / num_of_records
-    # match_scores_spread = cumulative_goals(team)['total'] / (num_of_records * 2)
 
-    def first_check(home_score, home_concede, home_total, away_score, away_concede, away_total):
-        average_home_score = _check(home_score)
-        average_home_concede = _check(home_concede)
-        average_home_total = _check(home_total)
-        record = {'score_prob' : average_home_score / 5 *100,
-                  'concede_prob': average_home_concede / 5 * 100,
-                  'over_prob': average_home_total / 5 * 100,
-        }
-        # the decisive part
-        return record
-    # to check for mutual encounters you could possibly go through the #odds link and extract the data.
-    # in retrieving the data you could read team records for home team and away team that have the same timestamp
+def check_over(scores):
+    """ input scores, output either 1 or 0"""
+    # if result is equal or greater than 3; then increment over counter else increment under counter
+    scores_diction = splitter(scores)
+    if scores_diction['home_goals'] + scores_diction['away_goals'] >= 3:
+        return 1
+    elif scores_diction['home_goals'] + scores_diction['away_goals'] <= 2:
+        return 0
 
+
+def one_x_2(scores):
+
+    # if home goals scored are more than the
+    # away goals increment home_counter else, draw or away_counter
+    scores = splitter(scores)
+    if scores['home_goals'] > scores['away_goals']:
+        return 1
+    elif scores['home_goals'] < scores['away_goals']:
+        return 0
+    else:
+        return 'x'
+
+
+def relative_win(scores, home_team, away_team):
+    """if one_teams goals are always above the others increment a counter too..."""
+    scores = splitter(scores)
+    home_goals = scores['home_goals']
+    away_goals = scores['away_goals']
+    if home_goals > away_goals:
+        return home_team
+    elif home_goals < away_goals:
+        return away_team
+
+
+def start_conveyer(match_dict):
+    # we first deal with over and under 25 patterns from mutual matches only, we
+    # will take the simplest approach
+    our_list = match_dict['mutual']
+
+    over_counter = 0
+    home_win_counter = 0
+    draw_counter = 0
+    away_win_counter = 0
+    home_team_counter = 0
+    away_team_counter = 0
+    for diction in our_list:
+        home_team_name = diction['home_team']
+        away_team_name = diction['away_team']
+        full_time_score = diction['full_time_score']
+        over_counter += check_over(full_time_score)
+        if one_x_2(full_time_score) == 1:
+            home_win_counter += 1
+        elif one_x_2(full_time_score) == 0:
+            draw_counter += 1
+        elif one_x_2(full_time_score) == 'x':
+            away_win_counter +=1
+        else:
+            raise Exception('Unrecognized returned Value for 1 x 2')
+        if relative_win(full_time_score, home_team_name, away_team_name) == home_team_name:
+            home_team_counter += 1
+        elif relative_win(full_time_score, home_team_name, away_team_name) == away_team_name:
+            away_team_counter += 1
+    overall = len(our_list)
+
+    over_25_percentage = over_counter / overall * 100
+    under_25_percentage = 100 - over_25_percentage
+    home_win_percentage = home_win_counter / overall * 100
+    draw_percentage = draw_counter / overall * 100
+    away_win_percentage = away_win_counter / overall * 100
+    home_team_percentage = home_team_counter / overall * 100
+    away_team_percentage = away_team_counter / overall * 100
+
+    # now the question remains how do we report the findings, below is a rudimentary way of how
+    # i will be doing it for now, i will improve on this model later
+    over_string = "\n\n OVER 25\n"
+    under_string = "\n\n UNDER 25\n"
+    if over_25_percentage > 80:
+        over_string += "{}  |  {}  |  {}  |  {}".format(match_dict['time'], match_dict['home_team'],
+                                          match_dict['away_team'], over_25_percentage)
+    if under_25_percentage > 80:
+        under_string += "{}  |  {}  |  {}  |  {}".format(match_dict['time'], match_dict['home_team'],
+                                          match_dict['away_team'], under_25_percentage)
+    if home_win_percentage > 80:
+        string1 = "home_pattern {} {} {} {}".format(match_dict['time'], match_dict['home_team'],
+                                          match_dict['away_team'], home_win_percentage)
+    if draw_percentage > 80:
+        string2 = "draw_pattern {} {} {} {}".format(match_dict['time'], match_dict['home_team'],
+                                          match_dict['away_team'], draw_percentage)
+    if away_win_percentage > 80:
+        string3 = "away_pattern {} {} {} {}".format(match_dict['time'], match_dict['home_team'],
+                                          match_dict['away_team'], away_win_percentage)
+    if home_team_percentage > 80:
+        string4 = "win_pattern {} >{}< {} {}".format(match_dict['time'], match_dict['home_team'],
+                                          match_dict['away_team'], home_team_percentage)
+    if away_team_percentage > 80:
+        string5 = "win_pattern {} {} >{}< {}".format(match_dict['time'], match_dict['home_team'],
+                                          match_dict['away_team'], away_team_percentage)
+    full_string = "%s\n %s\n %s\n %s\n %s\n %s\n %s\n" % (over_string, under_string, string1, string2,
+                                                          string3, string4, string5)
+    return full_string
 
 
 def get_specific_match_details(url):
     url = 'http://www.sportstats.com' + url
     full_page = requests.get(url)
-    home_dict = dict()
-    away_dict = dict()
+    match_dict = dict()
     insoup = BeautifulSoup(full_page.text, 'html.parser')
     country_league = insoup.find_all(id='center')
     country_league = country_league[0].find_all('div', class_='bread')
     country_league = country_league[0].find_all('a')
-    # country_league now contains 4 links, the first contains the website link, the
-    # second contains the game_type, third contains the country while the last contains the league
     league = country_league[len(country_league) - 1].get_text()
     country = country_league[len(country_league) - 2].get_text()
-    home_dict['country'] = away_dict['country'] = country
-    home_dict['league'] = away_dict['league'] = league
+
+    match_dict['country'] = country
+    match_dict['league'] = league
+
     teams = insoup.find('h1', class_='hidden').get_text()
     home_team = teams.split(' - ')[0]
     away_team = teams.split(' - ')[1]
-    home_dict['team'] = home_team
-    away_dict['team'] = away_team
+
+    match_dict['home_team'] = home_team
+    match_dict['away_team'] = away_team
+
     main_divs_info = insoup.find_all('div', class_='event-header-wrapper')
-    sub_div_info = list()
     for div in main_divs_info:
         home_logo_url = div.find_all('div', class_='home-logo')
-        # get img tag in above
         home_logo_src = home_logo_url[0].img.get('src')
-        home_dict['home_logo_src'] = home_logo_src
+        match_dict['home_logo_src'] = home_logo_src
         away_logo_url = div.find_all('div', class_='away-logo')
         away_logo_src = away_logo_url[0].img.get('src')
-        away_dict['away_logo_src'] = away_logo_src
-
-
-        scores_div = div.find_all('div', class_='event-header-score')[0]
+        match_dict['away_logo_src'] = away_logo_src
 
         result = parse_scores_for_match(insoup)
+        match_dict.update(result)
 
         div_date_time_string = div.find_all('span', class_='datet')[0].get_text()
-        # how the string looks Today, 26 Jun 2017, 00:00++++
         experimental_string = div_date_time_string
-        # i think the best solution is to employ regular expressions
         date_of_play = date_from_string(experimental_string)
         tarehe = date_of_play
-        # ??????*** try and convert tarehe to a timestamp ????*****
-        home_dict['timestamp'] = tarehe.timestamp()
-        away_dict['timestamp'] = tarehe.timestamp()
+        match_dict['time_of_play'] = tarehe.timestamp()
 
-    return [home_dict, away_dict]
+    return match_dict
+
 
 def date_from_string(string):
     # example : Today, 26 Jun 2017, 00:00++++
@@ -202,7 +246,7 @@ def date_from_string(string):
         _hour = int(tym[0])
         _minute = int(tym[1])
     else:
-        raise Exception('Unparsable date time format, please check that the wensite has not changed its time format.')
+        raise Exception('Unparsable date time format, please check that the website has not changed its time format.')
 
     # we now create the  final object that we send back
     date_of_play = datetime(_year, _month, _day, _hour, _minute)
@@ -215,6 +259,8 @@ def parse_scores_for_match(insoup):
     event_header_wrapper = insoup.find_all('div', class_='event-header-wrapper')[0]
     event_header_score = event_header_wrapper.find_all('div', class_='event_header-score')[0]
     full_time_score = event_header_score.span.get_text()
+    full_time_score = full_time_score.split(' - ')
+    full_time_score = '-'.join(full_time_score)
 
     # now for the half time and full time results
     full_score_info_div = event_header_wrapper.find_all('div', class_='full')
@@ -248,7 +294,7 @@ def score_validator(first_score, second_score, full_score):
     else:
         if len(first_score) != 3 and len(second_score) != 3 and len(full_score) != 3:
             raise Exception('There was a problem ')
-    full = int(full_score.split(' - '))
+    full = int(full_score.split('-'))
     first = int(first_score.split('-'))
     second = int(second_score.split('-'))
     if first[0] + second[0] == full[0] and first[1] + second[1] == full[1]:
@@ -256,3 +302,27 @@ def score_validator(first_score, second_score, full_score):
     else:
         return False
 
+def retrieve_mutual_matches_data(url):
+    """input the link as the get specific content function does
+    output: a dictionary of a single mutual_matches key with a list of dictionaries"""
+    url = 'http://www.sportstats.com' + url
+    full_page = requests.get(url)
+    soup = BeautifulSoup(full_page.text, 'html.parser')
+    sub_content = soup.find_all(id='subContent_0')[0]
+    mutual_block = sub_content.find_all(id='pos_21')[0]
+    maintainable_content = mutual_block.find_all(id='LS_maintableContent')[0]
+    table = maintainable_content.find_all(id='maintable_0')[0]
+    links_list = table.find_all('a',  class_='tabOdds')
+    all_hrefs = list()
+    for link in links_list:
+        href_text = link.get('href')
+        all_hrefs.append(href_text)
+
+    all_mutual_matches = list()
+    for href in all_hrefs:
+        mutual_match_instance = get_specific_match_details(href)
+        all_mutual_matches.append(mutual_match_instance)
+    # create a dictionary to be returned
+
+    mutual_diction = {'mutual': all_mutual_matches}
+    return mutual_diction
