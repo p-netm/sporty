0af759784d60d8d0a97c897f629e4bfd4b83947d
pmuriuki98 <pmuriuki98@gmail.com>
2017-11-10 04:22:56 -0500

    delete unnecessary files for new structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,24 @@
 """app Package constructor"""
 from flask import Flask
-from flask_bootstrap import Bootstrap
 from flask_mail import Mail
-from flask_moment import Moment  # not yet had an idea of how to implement this
 from flask_sqlalchemy import SQLAlchemy
-from ..config import config
-from .main import main as main_blueprint
+from config import config
 
 
-
-bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
 
 def create_app(configuration):
     app = Flask(__name__)
-    if not isinstance(configuration, str):
+    if not isinstance(configuration, str) and not in config.keys():
         raise TypeError('Configuration keys is not string')
     app.config.from_object(config[configuration])
 
-    bootstrap.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
 
