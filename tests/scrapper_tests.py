a2a93127b43cdd7684e52a0a5383f2dba09e09c9
pmuriuki98 <pmuriuki98@gmail.com>
2018-03-14 04:28:06 -0400

    scrapper module tests

--- tests/scrapper_tests.py
+++ tests/scrapper_tests.py
@@ -3,8 +3,9 @@
 """
 import unittest
 import os, BeautifulSoup, datetime
-from app.gears.scrapper import scrap_all_urls, date_from_string
+from app.gears.scrapper import scrap_all_urls, date_from_string, get_specific_match_details
 from validator import url
+from errors import *
 
 def create_file_path(filename):
 	"""abstract the path creation to the golden files"""
@@ -43,7 +44,9 @@ class ScrapperTest(unittest.TestCase):
 		# each scrapped element is a full blown valid url without the #odds
 		self.assertTrue(url(present_scrap[0]))
 		self.assertTrue(url(present_scrap[2]))
-		# should not have the fragment $odds at the end of the url
+		# should not have the fragment #odds at the end of the url
+		# we also do not create a beatiful soup object here as we do in the other test fixtures
+		# because the scrap_all_urls function does that by itself.
 		sample_url = present_scrap[0]
 		self.assertEqual(sample_url.find('#odds'), -1)
 		self.assertEqual(len(present_scrap), 200)
@@ -66,6 +69,104 @@ class ScrapperTest(unittest.TestCase):
 		self.assertIsInstance(date_from_string(sample_string)[0], datetime.date)
 		self.assertIsInstance(date_from_string(sample_string)[1], datetime.time)
 		
-	def test_get_specific_match_details(self):
-		"""This is more than a unit test"""
+	def assert_things(self, diction)
+		"""am not even sure that this works or even if this is legal"""
+		self.assertIn('league', diction.keys())
+		self.assertIn('country', diction.keys())
+		self.assertIn('home_team', diction.keys())
+		self.assertIn('away_team', diction.keys())
+		self.assertIn('date', diction.keys())
+		self.assertIn('time', diction.keys())
+		self.assertIn('home_first_half_goals', diction.keys())
+		self.assertIn('away_first_half_goals', diction.keys())
+		self.assertIn('home_second_half_goals', diction.keys())
+		self.assertIn('away_second_half_goals', diction.keys())
+		self.assertIn('home_match_goals', diction.keys())
+		self.assertIn('away_match_goals', diction.keys())
 		
+		
+	def test_get_specific_match_details_for_played_match(self):
+		"""This is more than a unit test"""
+		played_soup = BeautifulSoup(create_file_path('played.html'), 'html.parser')
+		result = get_specific_match_details(played_soup)
+		self.assert_things(result)
+		# type and value checks
+		self.assertEqual(result['league'], 'First Division')
+		self.assertEqual(result['country'], 'Cyprus')
+		self.assertEqual(result['home_team'], 'APOEL')
+		self.assertEqual(result['away_team'], 'AEK Larnaca')
+		self.assertEqual(result['date'], datetime.date(2018, 5, 11))
+		self.assertEqual(result['time'], datetime.time(14, 0))
+		self.assertEqual(result['home_first_half_goals'], 0)
+		self.assertEqual(result['home_second_half_goals'], 0)
+		self.assertEqual(result['away_first_half_goals'], 0)
+		self.assertEqual(result['away_second_half_goals'], 0)
+		
+		
+	def test_get_specific_match_details_for_abandoned_match(self):
+		"""do abandoned matches html files abide by the same formats as the rest of them"""
+		abandoned_soup = BeautifulSoup(create_file_path('abandoned.html'), 'html.parser')
+		result = get_specific_match_details(abandoned_soup)
+		self.assert_things(result)
+		# a brief check of value and types -> some attributes i wil not check since i do not expect them to have changed
+		self.assertEqual(result['date'], datetime.date(2018, 5, 11))
+		self.assertEqual(result['time'], datetime.time(17, 30))
+		self.assertEqual(result['home_first_half_goals'], 0)
+		self.assertEqual(result['home_second_half_goals'], 0)
+		self.assertEqual(result['away_first_half_goals'], 0)
+		self.assertEqual(result['away_second_half_goals'], 0)
+		
+		
+	def test_get_specific_match_details_for_penalties_match(self):
+		"""test fixture for future matches html files"""
+		penalty_soup = BeautifulSoup(create_file_path('penalties.html'), 'html.parser')
+		result = get_specific_match_details(penalty_soup)
+		self.assert_things(result)
+		# remember the system only recognizes result up until end of regulation time
+		# where the full scores defer from the adition of the respective half scores, then that means  that the 
+		# respective ficture was played into extra time.
+		self.assertEqual(result['league'], 'Czech Cup Women')
+		self.assertEqual(result['home_first_half_goals'], 2)
+		self.assertEqual(result['home_second_half_goals'], 0)
+		self.assertEqual(result['away_first_half_goals'], 0)
+		self.assertEqual(result['away_second_half_goals'], 2)
+		
+	def test_get_specific_match_details_for_postponed_match(self):
+		"""what about when we have a html file for a fixture that was postpponed"""
+		post_soup = BeautifulSoup(create_file_path('postponed.html'), 'html.parser')
+		result = get_specific_match_details(post_soup)
+		self.assert_things(result)
+		self.assertEqual(result['home_first_half_goals'], None)
+		self.assertEqual(result['home_second_half_goals'], None)
+		self.assertEqual(result['away_first_half_goals'], None)
+		self.assertEqual(result['away_second_half_goals'], None)
+		self.assertEqual(result['home_match_goals'], None)
+		sefl.assertEqual(result['away_match_goals'], None)
+		
+		#the test below requires a live connection to the internet to run.
+		
+# 	def test_retrieve_mutual_matches_data_for_played_match(self):
+# 		"""we just check that the mutual key has a value of the required format"""
+# 		played_soup = BeautifulSoup(create_file_path('played.html'), 'html.parser')
+# 		result = retrieve_mutual_matches_data(played_soup)
+# 		self.assertEqual(type(result), dict)
+# 		self.assertIn('mutual', result.keys())
+# 		self.assertIsInstance(result['mutual'], list)
+# 		self.assertIsInstance(result['mutual'][0], dict)
+# 		match_instance = result['mutual'][0]
+# 		self.assert_things(match_instance)
+
+	def test_deformed_mutual_matches_data(self):
+		"""what if the fed in html file does not have the data required"""
+		gold_soup = BeautifulSoup(create_file_path('gold.html'), 'html.parser')
+		result = retrieve_mutual_matches_data(gold_soup)
+		self.assertEqual(type(result), dict)
+		self.assertIn('mutual', result.keys())
+		self.assertIs(result['mutual'], None)
+		
+	def test_deformed_soccer_home_page(self):
+		"""lets see what is the responde of the scrap all links function when it has 
+		nothing to scrap"""
+		gold_soup = BeautifulSoup(create_file_path('gold.html'), 'html.parser')
+		with self.assertRaises(TagError):
+			scrap_all_urls(gold_soup)
