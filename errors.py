2521d6b2834d6c2e959ba58cc6ca985f18445412
pmuriuki98 <pmuriuki98@gmail.com>
2018-03-14 04:27:29 -0400

    id like to know errors that the system will throw at me

--- errors.py
+++ errors.py
@@ -1,9 +1,16 @@
 """Define custom Errors, its really just that basic"""
 
 TagError(Exception):
+	"""
+	This is raised when the system fails to locate a required vital 
+	html tag
+	"""
 	pass
 
 # validationError  as validation error
 
 PatternMatchError(Exception):
+	"""
+	happens when a regular expression fails to match a string that
+	the system expects should match"""
 	pass
