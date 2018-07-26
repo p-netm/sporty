5b894c12f5a86ef9e5ee285d45de93f8fd117a2a
= <pmuriuki98@gmail.com>
2018-07-26 20:29:04 +0300

    refactoring due to merge problems

--- app/gears/scrapper.py
+++ app/gears/scrapper.py
@@ -1,8 +1,4 @@
 import requests
-<<<<<<< HEAD
-import os
-=======
->>>>>>> 106c6f1de13e7616203fcad48f5c8f779fe02b43
 from bs4 import BeautifulSoup
 import datetime
 import re, os
@@ -11,99 +7,7 @@ from urllib.parse import urlparse, urljoin
 from errors import TagError, PatternMatchError
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-def stringify(a):
-    if len(str(a)) == 1:
-        return '0' + str(a)
-    return str(a)
-
-
-def scrap_for_mutual_matches(url):
-    """
-    {
-        "time": 12321321331,
-        "home_team":"team_name",
-        "away_team": "team_name",
-        "result": "2-5",
-        "first_half_score": "2-2",
-        "second_half_score": "0-5",
-        "past_home": [
-            {
-                "time": 12321321331,
-                "home_team":"team_name",
-                "away_team": "team_name",
-                "result": "2 - 5",
-                "first_half_score": "2-2",
-                "second_half_score": "0-5"
-            },
-            {
-                "time": 12321321331,
-                "home_team":"team_name",
-                "away_team": "team_name",
-                "result": "2 - 5",
-                "first_half_score": "2-2",
-                "second_half_score": "0-5"
-            }
-            ]
-        "past_away":[
-            {
-                "time": 12321321331,
-                "home_team":"team_name",
-                "away_team": "team_name",
-                "result": "2 - 5",
-                "first_half_score": "2-2",
-                "second_half_score": "0-5"
-            },
-            {
-                "time": 12321321331,
-                "home_team":"team_name",
-                "away_team": "team_name",
-                "result": "2 - 5",
-                "first_half_score": "2-2",
-                "second_half_score": "0-5"
-            }
-            ]
-        "mutual": [
-            {
-                "time": 12321321331,
-                "home_team":"team_name",
-                "away_team": "team_name",
-                "result": "2 - 5",
-                "first_half_score": "2-2",
-                "second_half_score": "0-5"
-            },
-            {
-                "time": 12321321331,
-                "home_team":"team_name",
-                "away_team": "team_name",
-                "result": "2 - 5",
-                "first_half_score": "2-2",
-                "second_half_score": "0-5"
-            },
-            {
-                "time": 12321321331,
-                "home_team":"team_name",
-                "away_team": "team_name",
-                "result": "2 - 5",
-                "first_half_score": "2-2",
-                "second_half_score": "0-5"
-            }
-        ]
-    }
-    """
-
-    webpage_text = requests.get(url).text
-    soup = BeautifulSoup(webpage_text, 'html.parser')
-    main_div = soup.find_all(id='pos_62')[0]
-    # main_div contains a div with the table that holds the match records
-    tbody_list = main_div.find_all('tbody')
-    td_list = list()  # will hold the td tags that hold the href with the #odds
-=======
-def _run_(url="""http://www.sportstats.com/soccer/matches/"""):
-=======
 def _run_(url):
->>>>>>> scrapper
     """The project manager, his main job will to motivate the other functions and  then consolidate their
     work into the last meaningful required product.
     :parameters: wil have an optional url of a certain soccer page from which to pull data from
@@ -142,122 +46,6 @@ def scrap_all_links(url):
     except IndexError as e:
         raise TagError('{}'.format('main_div#pos_62'))
 
-<<<<<<< HEAD
-    tbody_list = main_div.find_all('tbody') # tbody tags: sample rendering: Avai0 - 1 Hercilio Luz1.334.307.76
-    td_list = [] # will hold the td tags that hold the href with the #odds
->>>>>>> 106c6f1de13e7616203fcad48f5c8f779fe02b43
-    for tag in tbody_list:
-        td_list.extend(tag.find_all('a', class_='tabOdds'))
-
-    all_hrefs = []
-    for link in td_list:
-        all_hrefs.append(link.get('href'))
-<<<<<<< HEAD
-
-    for index in range(len(all_hrefs)):
-        link = all_hrefs[index]
-        # run the scrapper functions here for mutual matches and past matches here
-
-
-def splitter(scores):
-    """ returns the scores in the desired integer format"""
-    scores = scores.split('-')
-    home_goals = int(scores[0])
-    away_goals = int(scores[1])
-    return {'home_goals': home_goals, 'away_goals': away_goals}
-
-
-def check_over(scores_diction):
-    """ input scores, output either 1 or 0"""
-    # if result is equal or greater than 3; then increment over counter else increment under counter
-    if scores_diction['home_goals'] + scores_diction['away_goals'] >= 3:
-        return 1
-    elif scores_diction['home_goals'] + scores_diction['away_goals'] <= 2:
-        return 0
-
-
-def one_x_2(scores):
-
-    # if home goals scored are more than the
-    # away goals increment home_counter else, draw or away_counter
-    if scores['home_goals'] > scores['away_goals']:
-        return 1
-    elif scores['home_goals'] < scores['away_goals']:
-        return 0
-    else:
-        return 'x'
-
-
-def relative_win(scores, home_team, away_team):
-    """if one_teams goals are always above the others increment a counter too..."""
-    home_goals = scores['home_goals']
-    away_goals = scores['away_goals']
-    if home_goals > away_goals:
-        return home_team
-    elif home_goals < away_goals:
-        return away_team
-
-
-def start_conveyer(match_dict):
-    # we first deal with over and under 25 patterns from mutual matches only, we
-    # will take the simplest approach
-    our_list = match_dict['mutual']
-    if our_list is None:
-        return None
-    over_counter = 0
-    home_win_counter = 0
-    draw_counter = 0
-    away_win_counter = 0
-    home_team_counter = 0
-    away_team_counter = 0
-    overall = 0
-
-    for diction in our_list:
-        home_team_name = diction['home_team']
-        away_team_name = diction['away_team']
-        full_time_score = diction['full_time_score']
-        refactored_scores = splitter(full_time_score)
-
-        if full_time_score is not None:
-            over_counter += check_over(refactored_scores)
-            if one_x_2(refactored_scores) == 1:
-                home_win_counter += 1
-            elif one_x_2(refactored_scores) == 'x':
-                draw_counter += 1
-            elif one_x_2(refactored_scores) == 0:
-                away_win_counter +=1
-            else:
-                raise Exception('Unrecognized returned Value for 1 x 2')
-
-            if relative_win(refactored_scores, home_team_name, away_team_name) == home_team_name:
-                home_team_counter += 1
-            elif relative_win(refactored_scores, home_team_name, away_team_name) == away_team_name:
-                away_team_counter += 1
-            overall += 1
-
-    over_25_percentage = round(over_counter / overall * 100, 2)
-    under_25_percentage = round(100 - over_25_percentage, 2)
-    home_win_percentage = round(home_win_counter / overall * 100, 2)
-    draw_percentage = round(draw_counter / overall * 100, 2)
-    away_win_percentage = round(away_win_counter / overall * 100, 2)
-    home_team_percentage = round(home_team_counter / overall * 100, 2)
-    away_team_percentage = round(away_team_counter / overall * 100, 2)
-
-    # now the question remains how do we report the findings, below is a rudimentary way of how
-    # i will be doing it for now, i will improve on this model later
-    over_string = under_string = string1 = string2 = string3 = string4 = string5 = ''
-    full_string = None
-
-    checker = False
-
-
-def get_specific_match_details(url):
-    url = 'http://www.sportstats.com' + url
-    full_page = requests.get(url)
-=======
-        
-    refatored_hrefs = [urljoin('''http://www.sportstats.com''', urlparse(a).path) for a in all_hrefs]
-=======
     tbody_list = main_div.find_all('tbody')  # tbody tags: sample rendering: Avai0 - 1 Hercilio Luz1.334.307.76
     refactored_hrefs = []  # will hold the td tags that hold the href with the #odds
     for tag in tbody_list:
@@ -268,7 +56,6 @@ def get_specific_match_details(url):
             home_team = tag.find_all('td', class_='table-home')[0].find_all('a')[0].get_text()
             away_team = tag.find_all('td', class_='table-away')[0].find_all('a')[0].get_text()
             refactored_hrefs.append((home_team, away_team, a))
->>>>>>> scrapper
     return refactored_hrefs
 
 def process_league(league_name):
@@ -298,7 +85,6 @@ def get_specific_match_details(insoup):
     first_half_home_goals, first_half_away_goals, full_time_home-goals, 
     full_time_away_goals, date, time
     """
->>>>>>> 106c6f1de13e7616203fcad48f5c8f779fe02b43
     match_dict = dict()
     try:
         country_league = insoup.find_all(id='center')
@@ -358,48 +144,17 @@ def date_from_string(string):
         date_of_play = datetime.date(full_date.year, full_date.month, full_date.day)
         time_of_play = datetime.time(full_date.hour, full_date.minute)
     else:
-<<<<<<< HEAD
-<<<<<<< HEAD
-        raise Exception('Unparsable date time format, please check that the website has not changed its time format.')
-    # we now create the  final object that we send back
-    date_of_play = datetime(_year, _month, _day, _hour, _minute)
-    return date_of_play
-=======
-        raise PatternMatchError('Unparsable date time format, please check that the website has not changed its time format.')
-        
-    return day_of_play, time_of_play
->>>>>>> 106c6f1de13e7616203fcad48f5c8f779fe02b43
-=======
         raise PatternMatchError(
             'Unparsable date day format, please recheck that the website has not changed its date format.')
 
     return date_of_play, time_of_play
->>>>>>> scrapper
 
 
 def retrieve_scores(div):
     """:parameter: is a beautiful soup section of the website
     :returns: the hometeams half and full goals as well as those  of the awayteam's"""
     # there are three cases that this function should deal with
-<<<<<<< HEAD
-    main_divs_info = insoup.find_all('div', class_='event-header-wrapper')
-    div = main_divs_info[0]
-    div_date_time_string = div.find_all('span', class_='datet')[0].get_text()
-    experimental_string = div_date_time_string
-    date_of_play = date_from_string(experimental_string).timestamp()
-
-    if Time.time() < date_of_play:
-        # means future match and those no scores
-        return None
-    if date_of_play > (Time.time() - (2 * 60 *60)) and date_of_play < Time.time():
-        # means the game is currently in play
-        return None
-    # here we standardise the format regardless of the page display, follow the pattern [(]\d-\d , \d-\d[)]
-    event_header_wrapper = insoup.find_all('div', class_='event-header-wrapper')[0]
-    full_score_info_div = event_header_wrapper.find_all('div', class_='full')[1]
-=======
     full_score_info_div = div.find_all('div', class_='full')[1]
->>>>>>> 106c6f1de13e7616203fcad48f5c8f779fe02b43
     # now we format
     full_score_info = full_score_info_div.p.get_text()
     full_score_info = re.findall(r'\S-\S', full_score_info)
@@ -421,60 +176,6 @@ def parse_scores_for_match(div):
     returns a dictionary containing the full_time, first_half and second_half scores.
     :parameter: is a beautiful soup section of the website"""
     # input is a div tag that holds the results
-<<<<<<< HEAD
-    event_header_wrapper = insoup.find_all('div', class_='event-header-wrapper')[0]
-    event_header_score = event_header_wrapper.find_all('div', class_='event-header-score')[0]
-    full_time_score = event_header_score.span.get_text()
-    full_time_score = full_time_score.split(' - ')
-    full_time_score = '-'.join(full_time_score)
-
-    # now for the half time and full time results
-    full_score_info = retrieve_scores(insoup)
-
-    # validation of score data
-
-    pattern = r'[(]\d-\d , \d-\d[)]'
-    if full_score_info is not None:
-        if len(re.findall(pattern, full_score_info)) == 1:
-            scores = re.findall(pattern, full_score_info)[0]
-            first_half_scores_pattern = r'[(](\d-\d) , \d-\d[)]'
-            first_half_scores = re.findall(first_half_scores_pattern, scores)[0]
-            second_half_scores_pattern = r'[(]\d-\d , (\d-\d)[)]'
-            second_half_scores = re.findall(second_half_scores_pattern, scores)[0]
-        else:
-            raise Exception('The score format for half time , full time seems to have changed')
-
-    else:
-        return {'full_time_score': None, 'first_half_score': None,
-                'second_half_score': None}
-    # how about a little validation. we do so by checking that the scores equal to full-time scores
-    # validate only when full_tim_score is length is 1
-
-    if len(full_time_score) == 3:
-        score_validated = score_validator(first_half_scores, second_half_scores, full_time_score)
-    else:
-        return {'full_time_score': full_time_score, 'first_half_score': first_half_scores,
-                'second_half_score': second_half_scores}
-    if score_validated:
-        # return the data in a parsable format
-        return {'full_time_score': full_time_score, 'first_half_score': first_half_scores,
-                'second_half_score': second_half_scores}
-    else:
-        raise Exception('Problem validating the Scores.')
-
-
-def score_validator(first_score, second_score, full_score):
-    if not isinstance(first_score, str) or not isinstance(second_score, str) or not isinstance(full_score,str):
-        raise TypeError('One of the argument scores is in an unrecognizable format')
-    else:
-        if len(first_score) != 3 and len(second_score) != 3 and len(full_score) != 3:
-            raise Exception('There was a problem ')
-    full = (full_score.split('-'))
-    first = (first_score.split('-'))
-    second = (second_score.split('-'))
-    if int(first[0]) + int(second[0]) == int(full[0]) and int(first[1]) + int(second[1]) == int(full[1]):
-        return True
-=======
     event_header_score = div.find_all('div', class_='event-header-score')[0]
     full_time_score = event_header_score.span.get_text()
     # if we have a valid full time score then we can be sure that we have the scores otherwise nope.
@@ -486,13 +187,8 @@ def score_validator(first_score, second_score, full_score):
         away_goals = int(result[1])
 
         # now for the half time and full time results
-<<<<<<< HEAD
-        home_first_half_goals, away_first_half_goals, home_second_half_goals, away_second_half_goals = retrieve_scores(div)
->>>>>>> 106c6f1de13e7616203fcad48f5c8f779fe02b43
-=======
         home_first_half_goals, away_first_half_goals, home_second_half_goals, away_second_half_goals = retrieve_scores(
             div)
->>>>>>> scrapper
     else:
         home_goals, away_goals = None, None
         home_first_half_goals, away_first_half_goals, home_second_half_goals, away_second_half_goals = None, None, None, None
@@ -506,12 +202,6 @@ def score_validator(first_score, second_score, full_score):
         'away_second_half_goals': away_second_half_goals
     }
 
-<<<<<<< HEAD
-def retrieve_mutual_matches_data(url):
-    """input the link as the get specific content function does
-    output: a dictionary of a single mutual_matches key with a list of dictionaries"""
-=======
->>>>>>> 106c6f1de13e7616203fcad48f5c8f779fe02b43
 
 def retrieve_mutual_matches_data(soup):
     """
--- app/models.py
+++ app/models.py
@@ -1,183 +1,18 @@
-<<<<<<< HEAD
-from . import db
-
-
-=======
 """Definition of all the class models that wil be mapped onto a daabase
 unlike for the eanmble ts, te eanmble sp will be a bit different due the increased level of operations and 
 data manipulations involved in getting to a reasonable fixture prediction analysis
 """
 from . import db
 
-<<<<<<< HEAD
->>>>>>> 106c6f1de13e7616203fcad48f5c8f779fe02b43
-=======
 
->>>>>>> scrapper
 class Team(db.Model):
     """Represents the data that will be saved about a specific team
 
     a representation of the aspects that are most important in this preliminary
     survey, this aspects will be added to the data csv file
     """
-<<<<<<< HEAD
-<<<<<<< HEAD
-    __tablename__ = 'Teams'
-    team_id = db.Column(db.Integer, primary_key=True)
-    team_name = db.Column(db.String(100))
-    date_founded = db.Column(db.DATE, nullable=True)
-    logo = db.Column(db.TEXT, nullable=True)
-    nick_name = db.Column(db.String(100), nullable=True)
-    short_name = db.Column(db.String(30), nullable=True)
-    website = db.Column(db.String(500), nullable=True)
-    kits = db.Column(db.String(500), nullable=True)
-
-    # id = Column(Integer, primary_key=True)
-    # time = Column(Integer)
-    # team_name = Column(String)
-    # country = Column(String)
-    # league = Column(String)
-    # goals_conceded = Column(Integer)
-    # goals_scored = Column(Integer)
-    # logo = Column(String)
-    # flagged = Column(Integer, default=0)
-    # # types of checkers that we will be using: over, under, win, loss, draw,
-    # ou_25 = Column(Boolean)
-    # result = Column(Enum('1', 'x', '2'))
-
-
-    def __init__(self, team_name, time, country_name, league_name, goals_scored, goals_conceded, logo):
-
-        if type(country_name) is not str:
-            raise TypeError('The Country_Name')
-        self.country = country_name
-
-        if not isinstance(team_name, str):
-            raise TypeError('The Team_name is not a String')
-        self.team_name = team_name
-
-        if not isinstance(league_name, str):
-            raise TypeError('unrecognized format for league name')
-        self.league = league_name
-
-        if not isinstance(logo, str):
-            raise TypeError('Logo is not of a valid type')
-        self.logo = logo
-
-        if not isinstance(goals_conceded, int) or not isinstance(goals_scored, int):
-            raise TypeError('Unrecognized format for both goals scored and goals conceded')
-        self.goals_conceded = goals_conceded
-        self.goals_scored = goals_scored
-
-        self.time = time
-
-    def __repr__(self):
-        """formats a string into an arbitrary string presentation"""
-        return '<{}{}{}{}{}{}{}>'.format(self.id, self.team_name, self.time, self.country,
-                                self.league, self.goals_scored, self.goals_conceded)
-
-    # class Continent(db.Model):
-    #     __tablename__ = 'Continents'
-    #
-    #     continent_name = db.Column(db.String(45), primary_key=True)
-    #     color_code = db.Column(db.String(10), unique=True)
-    #     size = db.Column(db.Integer)
-    #     population = db.Column(db.Integer)
-    #
-    #     def __init__(self, continent_name, color_code, size, population):
-    #         self.continent_name = continent_name
-    #         self.color_code = color_code
-    #         self.size = size
-    #         self.population = population
-
-
-    class Country(db.Model):
-        __tablename__ = 'Countries'
-
-        country_code = db.Column(db.String(10), primary_key=True)
-        country_name = db.Column(db.String(150), unique=True)
-        color_code = db.Column(db.String(10))
-        size = db.Column(db.Integer)
-        population = db.Column(db.Integer)
-        # currency = db.Column(db.String(30))
-        # coat_of_arms = db.Column(db.String())
-        flag_picture = db.Column(db.String(100))
-        # gdp = db.Column(db.DECIMAL(10,2))
-        # continent_name  = db.Column(db.String(45), db.ForeignKey('Continents.continent_name'))
-        #
-        # continent = db.relationship('Continent', back_populates='Countries')
-
-        def __init__(self, country_code, country_name, continent_name):
-            self.country_code = country_code
-            self.country_name = country_name
-            self.continent_name = continent_name
-
-
-    # class Team(db.Model):
-    #     __tablename__ = 'Teams'
-    #
-    #
-    #     # league_id = db.Column(db.String(10))
-    #     # manager_name = db.Column(db.String(45))
-
-
-    class Match(db.Model):
-        __tablename__ = 'Matches'
-
-        match_id = db.Column(db.String(20), primary_key=True)
-        date = db.Column(db.DATE, nullable=False)
-        time = db.Column(db.TIME, nullable=False)
-        team1 = db.Column(db.Integer, db.ForeignKey('Teams.team_id'))
-        team2 = db.Column(db.Integer, db.ForeignKey('Teams.team_id'))
-        # refa = db.Column(db.String(100))
-        # season = db.Column(db.String(10))
-        # averaged_match_odds = db.Column(DECIMAL, db.ForeignKey(''))
-        results_id = db.Column(db.String(10), db.ForeignKey('Results.result_id'))
-        # stadium_name = db.Column(db.String(150), db.ForeignKey('Stadiums.stadium_name'), nullable=True)
-
-    class League(db.Model):
-        __tablename__ = 'leagues'
-
-        league_id = db.Column(db.String(20), primary_key=True)
-
-
-    # class Result(db.Model):
-    #     __tablename__ = "Results"
-    #
-    #     result_id = db.Column(db.String(10), primary_key=True)
-    #     full_time_home_goals = db.Column(db.Integer)
-    #     full_time_away_goals = db.Column(db.Integer)
-    #     half_time_home_goals = db.Column(db.Integer)
-    #     half_time_away_goals = db.Column(db.Integer)
-
-    # class Stadium(db.Model):
-    #     __tablename__ = 'Stadiums'
-    #
-    #     stadium_name = db.Column(db.String(100), primary_key=True)
-    #     stadium_code = db.Column(db.Integer)
-    #     capacity = db.Column(db.String())
-    #     country_code = db.Column(db.String(10), db.ForeignKey('Countries.country_code'))
-    #     team_id = db.Column(db.Integer, db.ForeignKey('Teams.team_id'))
-
-        # country = db.relationship('Country', back_populates='Stadiums')
-
-    # class Prediction(db.Model):
-    #     __tablename__ = 'Predictions'
-    #
-    #     prediction_id = db.Column(db.Integer, primary_key=True)
-    #     probability = db.Column(db.Integer)
-    #     match_id = db.Column(db.String(20), db.ForeignKey('Matches.match_id'))
-    #     bet_id = db.Column(db.String(10), db.ForeignKey('Bets.bet_id'))
-=======
-
-    team_id = db.Column(db.Integer, primary_key=True)
-    team_name = db.Column(db.String())
-    league_id = db.Column(db.Integer, db.ForeignKey('league.league_id'), nullable=False)
-    logo = db.Column(db.String())
-=======
     team_name = db.Column(db.String(), unique=True, primary_key=True)
     logo = db.Column(db.String(), nullable=True)  # can set unique to true
->>>>>>> scrapper
     # the below flag attributes are for top-teams section
     over = db.Column(db.Boolean(), nullable=False, default=False)
     gg = db.Column(db.Boolean(), nullable=False, default=False)
@@ -278,11 +113,6 @@ class Flagged(db.Model):
     
     def __repr__(self):
         """This is pretty basic"""
-<<<<<<< HEAD
-        return '<{} {} {} {} {}>'.format(self.flag_id, self.team_one, self.team_two, self.date, self.time)
-    
->>>>>>> 106c6f1de13e7616203fcad48f5c8f779fe02b43
-=======
         return '<{} {} {} {} {} 1.{} x.{} 2.{}>'.format(self.flag_id, self.team_one, self.team_two, self.date, self.time,
                                                         self._1, self._x, self._2)
 
@@ -292,4 +122,3 @@ class SubscribedEmail(db.Model):
     def __repr__(self):
         return '<email {}>'.format(self.email)
     
->>>>>>> scrapper
